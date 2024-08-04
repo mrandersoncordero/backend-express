6db0fa2943781c0498ec087cdc02f44eb4c8a051
mrandersoncordero <acorderofigueroa7@gmail.com>
2024-08-04 17:02:12 -0400

    ambiar index

--- api/index.js
+++ api/index.js
@@ -1,27 +1,24 @@
 const express = require('express');
-const routerApi = require('./routes/index');
+const routerApi = require('./routes');
 const cors = require('cors');
-
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/errorHandler');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3001;
 
 app.use(express.json());
-// app.use(cors()); // dar acceso a todos
 
-const whitelist = ['http://localhost:8000', 'http://localhost:5500'];
+const whitelist = ['http://localhost:8000', 'http://localhost:5500', 'http://127.0.0.1:5500'];
 const options = {
   origin: (origin, callback) => {
-    if(whitelist.includes(origin)) {
-      callback(null, true)
-    }else {
-      callback(new Error('no permitido'));
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('No permitido por CORS'));
     }
   }
-}
+};
 app.use(cors(options));
-
 
 app.get('/', (req, res) => {
   res.send('Hola mi server en express');
@@ -29,7 +26,7 @@ app.get('/', (req, res) => {
 
 routerApi(app);
 
-// uso de middlewares
+// Uso de middlewares
 app.use(logErrors);
 app.use(boomErrorHandler);
 app.use(errorHandler);
@@ -37,3 +34,6 @@ app.use(errorHandler);
 app.listen(port, () => {
   console.log(`Servidor corriendo en el puerto ${port}`);
 });
+
+// Exportar la aplicación para Vercel
+module.exports = app;
